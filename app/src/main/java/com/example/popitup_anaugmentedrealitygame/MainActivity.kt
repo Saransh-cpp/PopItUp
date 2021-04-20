e77fed956fc923c8d79c84de0d3e54bb33c9d2ac
Saransh <saransh0701@gmail.com>
2021-04-20 22:58:49 +0530

    Bottom Navigation Bar done

--- app/src/main/java/com/example/popitup_anaugmentedrealitygame/MainActivity.kt
+++ app/src/main/java/com/example/popitup_anaugmentedrealitygame/MainActivity.kt
@@ -2,27 +2,41 @@ package com.example.popitup_anaugmentedrealitygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bottomNav.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
+
+        val homeFragment = HomeFragment()
+        val leaderboardFragment = LeaderboardFragment()
+
+        replaceFragment(homeFragment)
+
+        bottomNav.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
                 R.id.menu_home -> {
-                    setContent("Home")
-                    true
+                    replaceFragment(homeFragment)
                 }
                 R.id.menu_leaderboard -> {
-                    setContent("Notification")
-                    true
+                    replaceFragment(leaderboardFragment)
                 }
-                else -> false
             }
+            true
         }
     }
 
-    private fun setContent(content: String) {
+    private fun replaceFragment(fragment: Fragment) {
+        if (fragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.container, fragment)
+            transaction.commit()
+        }
+
     }
 }
